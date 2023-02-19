6f24224760e8ed2a7d5972706e0e6c5b75e9f72d
MahbubAhmedTonmoy <38882962+MahbubAhmedTonmoy@users.noreply.github.com>
2023-02-19 22:04:34 +0600

    hoc and render props pattern

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,14 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { AuthProvider } from "../contexts/AuthContext";
 import "../styles/App.css";
+import PrivateOutlet from "./hoc/PrivateOutlet";
+import PublicRoute from "./hoc/PublicRoute";
 import Layout from "./Layout";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Quiz from "./pages/Quiz";
 import Result from "./pages/Result";
 import Signup from "./pages/Signup";
-
-import { AuthProvider } from "../contexts/AuthContext";
 function App() {
   return (
     <Router>
@@ -15,10 +16,14 @@ function App() {
         <Layout>
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/signup" element={<Signup />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/quiz" element={<Quiz />} />
-            <Route path="/result" element={<Result />} />
+            <Route path="/" element={<PublicRoute />}>
+              <Route path="/signup" element={<Signup />} />
+              <Route path="/login" element={<Login />} />
+            </Route>
+            <Route path="/*" element={<PrivateOutlet />}>
+              <Route path="quiz" element={<Quiz />} />
+              <Route path="result" element={<Result />} />
+            </Route>
           </Routes>
         </Layout>
       </AuthProvider>
